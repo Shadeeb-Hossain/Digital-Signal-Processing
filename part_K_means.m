be307bf882441745ed4ed84795f09423ea1fcbeb
Shadeeb Hossain <shadeebhossain@proton.me>
2024-03-13 22:11:21 -0400

    Add files via upload

--- part_K_means.m
+++ part_K_means.m
@@ -1,19 +1,10 @@
 close all;
 clear all;
 clc;
-% Read the noisy speech signal
 [s, Fs] = audioread('noisy.wav');
-
-% Define the length of the moving average filter
-K = 100; % You can adjust this value to find the optimal balance between noise reduction and speech clarity
-
-% Apply the moving average filter
+K = 100; 
 filtered_signal = movmean(s, K);
-
-% Save the filtered signal as an audio file
 audiowrite('filtered_output.wav', filtered_signal, Fs);
-
-% Plot the original and filtered signals for comparison
 t = (0:length(s)-1) / Fs;
 figure;
 subplot(2,1,1);
